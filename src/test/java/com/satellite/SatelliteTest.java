01e63df3a364e6689bc65fab676c9ae0a91b9c58
Marc-Andre <224@live.ca>
2017-11-21 16:04:47 -0500

    Add Satellite Test

---
+++ src/test/java/com/satellite/SatelliteTest.java
@@ -0,0 +1,34 @@
+package com.satellite;
+
+import static org.junit.Assert.*;
+
+public class SatelliteTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void create() throws Exception {
+    }
+
+    @org.junit.Test
+    public void read() throws Exception {
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+    }
+
+    @org.junit.Test
+    public void delete() throws Exception {
+    }
+
+    @org.junit.Test
+    public void push() throws Exception {
+    }
+
+}
